aeac9f1f923907621cfc62c948d5ed018205ae4b
Orkun Firat Balkanci <orkun@unumotors.com>
2020-05-06 10:14:37 +0000

    Update worker to handle cancel message from the broker (CLOUD-1618)

---
+++ test/helper/util.js
@@ -0,0 +1,18 @@
+
+// checks the given condition
+// every 100 milliseconds as the default frequency.
+// this check would hang if tests are not run with --timeout
+const waitUntil = async(condition, frequency = 100) => {
+  await new Promise(resolve => {
+    const interval = setInterval(() => {
+      if (condition()) {
+        clearInterval(interval)
+        resolve()
+      }
+    }, frequency)
+  })
+}
+
+module.exports = {
+  waitUntil
+}
--- test/queue-worker.test.js
+++ test/queue-worker.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable require-await */
 /* eslint-disable global-require */
 const test = require('ava')
+const { waitUntil } = require('./helper/util')
 
 process.env.NODE_ENV = 'development' // Overwrite ava to be able to unit test
 process.env.TAUBE_DEBUG = true
@@ -173,6 +174,54 @@ test.serial('a worker can only "consume" once', async t => {
   const worker1 = new taube.Worker({ key })
   await worker1.consume(() => {})
   await t.throwsAsync(() => worker1.consume(() => {}), { message: 'There can only be one "consume"er per Worker' })
+})
+
+test.serial('worker does re-setup if queue is deleted', async t => {
+  t.plan(7)
+  globalTestCounter++
+  const key = `test-key${globalTestCounter}`
+  const queue = new taube.Queue({ key })
+  const worker1 = new taube.Worker({ key })
+  const dataPackage1 = { test: 1, data: { data: 1 } }
+  const dataPackage2 = { test: 2, data: { data: 2 } }
+  let resolve1
+  let promise1 = new Promise(async resolve => {
+    resolve1 = resolve
+  })
+
+  let count = 0
+  let finishedConsumer1 = false
+  let finishedConsumer2 = false
+  let initialWorkerTag
+  await worker1.consume(async(data) => {
+    count++
+    if (count == 1) {
+      t.is(finishedConsumer2, false)
+      t.is(data.test, count)
+      t.deepEqual(data, dataPackage1)
+      finishedConsumer1 = true
+      initialWorkerTag = worker1.consumer.consumerTag // initial consumer tag to compare later
+    }
+    if (count == 2) {
+      t.is(finishedConsumer1, true)
+      t.is(data.test, count)
+      t.deepEqual(data, dataPackage2)
+      finishedConsumer2 = true
+      t.not(initialWorkerTag, worker1.consumer.consumerTag, `worker should have a new consumer tag after re-setup`)
+      resolve1()
+    }
+  })
+
+  await queue.enqueue(dataPackage1)
+
+  // This will cancel all consumers attached to this queue
+  // eslint-disable-next-line no-underscore-dangle
+  await queue.channel._channel.deleteQueue(queue.keyEscaped)
+
+  // Wait for the worker1 to re-setup consumer
+  await waitUntil(() => worker1.consumer && initialWorkerTag != worker1.consumer.consumerTag)
+  await queue.enqueue(dataPackage2)
+  await promise1
 })
 
 test.after(async() => {
