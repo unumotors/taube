e7555b41dae0360073475aad447d6d033f29083a
Anvita Mishra <anvita@unumotors.com>
2021-04-21 07:56:51 +0000

    Add env variable to set JSON size limit

---
+++ test/json-size.test.js
@@ -0,0 +1,40 @@
+/* eslint-disable require-await */
+const test = require('ava')
+const Joi = require('joi')
+const fs = require('fs').promises
+const path = require('path')
+
+process.env.NODE_ENV = 'development' // Overwrite ava to be able to unit test
+process.env.TAUBE_UNIT_TESTS = true
+process.env.TAUBE_JSON_SIZE_LIMIT = '150kb'
+
+const taube = require('../lib')
+
+let port
+test.before(async() => {
+  await taube.http.listen() // this makes unit test cover 100%
+  port = taube.http.getPort()
+})
+
+test('Should be able to process larger payloads based on express json config', async(t) => {
+  const file = await fs.readFile(path.resolve('./test/test-files/200kb.json'))
+  const payload = JSON.parse(file)
+
+  const server = new taube.Server({})
+  server.post(
+    `/test-file-upload-limit/upload`,
+    {
+      body: Joi.object().keys({
+        payload: Joi.array()
+      })
+    },
+    async(req) => {
+      t.deepEqual(req.body.payload[0], payload[0])
+      return { success: true }
+    }
+  )
+  const client = new taube.Client({ uri: 'http://localhost', port })
+  const res = await client.post(`/test-file-upload-limit/upload`, { payload })
+
+  t.deepEqual(res, { success: true })
+})
